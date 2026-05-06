a669df61ffc06d859effa47989cf9ea8a1a160ca
Roba <roba.ahmed211@gmail.com>
2026-05-06 22:11:40 +0300

    Fully working AI

--- logic/ai.py
+++ logic/ai.py
@@ -1,6 +1,6 @@
 from .state import GameState
 from constants import (ATTACKER, DEFENDER, DIRECTIONS, DEFENDER_PLAYER, POINTS_FOR_SURROUNDING,
-                        POINTS_FOR_CORNER_DISTANCE)
+                        POINTS_FOR_CORNER_DISTANCE, ATTACKER_PLAYER, DEFENDER_PLAYER, INF, STEP_FACTOR)
 from .rules import get_possible_moves, is_valid_move
 # defender is min, attacker is max
 
@@ -12,9 +12,10 @@ class AI_Player:
 
     # Uses distance from corner for minimizing player
     # Uses number of attackers around the king for maximizing player
-    def evaluate(self, isMaximizing: bool, gameState: GameState) -> int:
+    def evaluate(self, isMaximizing: bool, gameState: GameState, steps: int) -> int:
         if (gameState.check_game_over()):
-            return float('inf') if gameState.winner == ATTACKER else -float('inf')
+            steps *= STEP_FACTOR
+            return (INF - steps) if gameState.winner == ATTACKER else (-INF + steps)
         
         kingI, kingJ = gameState.get_king_position()
         score = 0
@@ -24,7 +25,7 @@ class AI_Player:
                     or gameState.is_restricted(direction[0] + kingI, direction[1] + kingJ)):
                     score += POINTS_FOR_SURROUNDING
 
-        distance = float('inf')
+        distance = INF
         # Get distance between king and nearest corner
         for corner in gameState.corners:
             distance = min(distance, (abs(kingI - corner[0]) + abs(kingJ - corner[1])))
@@ -40,23 +41,28 @@ class AI_Player:
         return score
         
     # Returns all moves that can be done by the player
-    def get_moves(self, gameState) -> list[list[tuple[int, int]]]:
-        player = gameState.current_player
-        pieces = gameState.getDefenderPieces() if player == DEFENDER_PLAYER else gameState.getAttackerPieces()
+    def get_moves(self, gameState, isMaximizing) -> list[list[tuple[int, int]]]:
+        # player = gameState.current_player
+        pieces = gameState.getAttackerPieces() if isMaximizing else gameState.getDefenderPieces()
         moves = []
         for i, j in pieces:
+            # print(f"Pos ({i}, {j}): can go to {get_possible_moves(gameState, (i, j))}")
             for move in get_possible_moves(game_state=gameState, position=(i, j)):
                 moves.append([(i, j), move])
+        #print(f"Moves: {moves}")
         return moves
             
     def alphabeta(self, gameState, depth, alpha, beta, isMaximizing):
-        if (gameState.check_game_over or depth == 0):
-            return self.evaluate(isMaximizing, gameState)
+        if (gameState.check_game_over() or depth == 0):
+            return self.evaluate(isMaximizing, gameState, self.depth - depth)
 
         if isMaximizing:
-            maxValue = -float('inf')
-            for move in self.get_moves(gameState):
+            maxValue = -INF
+            for move in self.get_moves(gameState, isMaximizing):
+                # print(f"Old game state:\n{gameState}")
                 newState = gameState.getResultingState(oldLocation=move[0], newLocation=move[1])
+                # player = "A" if isMaximizing else "D"
+                # print(f"Player: {player} from {move[0]} to {move[1]} New state:\n{newState}")
                 tmp = self.alphabeta(newState, depth - 1, alpha, beta, False)
                 maxValue = max(maxValue, tmp)
 
@@ -65,8 +71,8 @@ class AI_Player:
                     break
             return maxValue
         else:
-            minValue = float('inf')
-            for move in self.get_moves(gameState):
+            minValue = INF
+            for move in self.get_moves(gameState, isMaximizing):
                 newState = gameState.getResultingState(oldLocation=move[0], newLocation=move[1])
                 tmp = self.alphabeta(newState, depth - 1, alpha, beta, True)
                 minValue = min(minValue, tmp)
@@ -76,16 +82,28 @@ class AI_Player:
                     break
             return minValue
 
-    def getBestMove(self, gameState) -> tuple[int, int]:
-        bestScore = -float('inf')
+    def getBestMove(self, gameState) -> list[tuple[int, int]]:
+        bestScore = -INF if self.role == ATTACKER_PLAYER else INF
         # bestMove is a list of 2 tuples, the first is the initail position, the second is the new position
         bestMove = []
-        for move in self.get_moves(gameState):
-            newState = gameState.getResultingState(oldLocation=move[0], newLocation=move[1])
-            score = self.alphabeta(gameState=gameState, depth=self.depth - 1,
-                                    alpha=-float('inf'), beta=float('inf'), isMaximizing=True)
-            if score > bestScore:
-                bestScore = score
-                bestMove = move
 
+        for move in self.get_moves(gameState, self.role == ATTACKER_PLAYER):
+            newState = gameState.getResultingState(oldLocation=move[0], newLocation=move[1])
+            # Attacker is maximizing
+            if self.role == ATTACKER_PLAYER:
+                score = self.alphabeta(gameState=newState, depth=self.depth - 1,
+                                    alpha=-INF, beta=INF, isMaximizing=False)
+                if score > bestScore:
+                    bestScore = score
+                    bestMove = move
+            # Defender is minimizing
+            elif self.role == DEFENDER_PLAYER:
+                score = self.alphabeta(gameState=newState, depth=self.depth - 1,
+                                    alpha=-INF, beta=INF, isMaximizing=True)
+                if score < bestScore:
+                    bestScore = score
+                    bestMove = move
+            
+
+        print(f"Best move: {bestMove}, score: {score}")
         return bestMove
